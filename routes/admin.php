7eb0c8ba74abc603e7c72fe7ee23ea9eb3bfde36
agentepeke <agentepeke@gmail.com>
2025-10-23 21:50:56 -0600

    Add: Agregando y configurando el sidebar y navigation

--- routes/admin.php
+++ routes/admin.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
-    return "Hola admin!";
-})->name('home');
+    return view('admin.dashboard');
+})->name('dashboard');
